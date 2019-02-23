fe215389d4beab3329e08dbfa9d330763097d7cc
Philippe Chartrand <philippe.chartrand@videotron.ca>
2019-02-23 18:29:08 +0000

    keep only current week data

--- tests/tests_measurements.py
+++ tests/tests_measurements.py
@@ -1,30 +1,53 @@
 from unittest import TestCase
 from thermostat.measurements import Measurements
+import datetime
+
+
+class NextMinuteIterator(object):
+    def __init__(self):
+        self.now = datetime.datetime.now()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        now = self.now
+        self.now += datetime.timedelta(minutes=1)
+        return now
 
 
 class MeasurementsTests(TestCase):
     def test_can_add_a_measurement(self):
+        di = NextMinuteIterator()
         m = Measurements(10)
-        m.add(dict(time=1, value=10.0))
-        m.add(dict(time=2, value=11.0))
+        m.add(dict(time=di.next(), value=10.0))
+        m.add(dict(time=di.next(), value=11.0))
 
-        self.assertEqual(2, m.len())
+        self.assertEqual(2, len(m))
 
 
     def test_can_constrain_measurement_array(self):
+        di = NextMinuteIterator()
+        d1 = di.next()
+        d2 = di.next()
+        d3 = di.next()
         m = Measurements(2)
-        m.add(dict(time=1, value=10.0))
-        m.add(dict(time=2, value=11.0))
-        m.add(dict(time=3, value=12.0))
+        m.add(dict(time=d1, value=10.0))
+        m.add(dict(time=d2, value=11.0))
+        m.add(dict(time=d3, value=12.0))
 
-        self.assertEqual(2, m.len())
-        self.assertEqual(dict(time=2,value=11.0), m.measurements[0])
+        self.assertEqual(2, len(m))
+        self.assertEqual(dict(time=d2,value=11.0), m.measurements[0])
 
 
     def test_can_iterate_on_measurements(self):
-        m1 = dict(time=1, value=10.0)
-        m2 = dict(time=2, value=11.0)
-        m3 = dict(time=3, value=12.0)
+        di = NextMinuteIterator()
+        d1 = di.next()
+        d2 = di.next()
+        d3 = di.next()
+        m1 = dict(time=d1, value=10.0)
+        m2 = dict(time=d2, value=11.0)
+        m3 = dict(time=d3, value=12.0)
 
         m = Measurements(3)
         m.add(m1)
@@ -38,3 +61,30 @@ class MeasurementsTests(TestCase):
         self.assertEqual(measurements[0], m1)
         self.assertEqual(measurements[1], m2)
         self.assertEqual(measurements[2], m3)
+
+    def test_can_remove_old_values(self):
+
+        now = datetime.datetime.now()
+        m = Measurements(5)
+        monday = m.week_start()
+        m1 = dict(time=monday - datetime.timedelta(days=1), value=10)
+        m2 = dict(time=monday, value=11)
+        m3 = dict(time=monday +  datetime.timedelta(hours=1), value=12)
+        m4 = dict(time=now, value=13)
+        m5 = dict(time=now + datetime.timedelta(days=5), value=14)
+
+        m.add(m1)
+        m.add(m2)
+        m.add(m3)
+        m.add(m4)
+        m.add(m5)
+
+        self.assertEqual(3, len(m))
+
+        measurements = []
+        for measurement in m:
+            measurements.append(measurement)
+
+        self.assertEqual(measurements[0], m3)
+        self.assertEqual(measurements[1], m4)
+        self.assertEqual(measurements[2], m5)
--- thermostat/measurements.py
+++ thermostat/measurements.py
@@ -1,17 +1,35 @@
+import datetime
+
 class Measurements(object):
     def __init__(self, max):
         self.MAX = max
         self.measurements = []
 
+    @staticmethod
+    def week_start():
+        current_time = datetime.datetime.now()
+        return current_time - datetime.timedelta(
+            hours=current_time.hour,
+            minutes=current_time.minute,
+            days=current_time.weekday()
+        )
+
+    def delete_last_week(self):
+        monday = self.week_start()
+        for measurement in self.measurements[:]:
+            if measurement['time'] < monday:
+                self.measurements.remove(measurement)
+
     def add(self, measurement):
+        self.delete_last_week()
         self.measurements.append(measurement)
         self.trim()
 
-    def len(self):
+    def __len__(self):
         return len(self.measurements)
 
     def trim(self):
-        l = self.len()
+        l = len(self)
         if l > self.MAX:
             self.measurements = self.measurements[l - self.MAX:]
 
