9ef2597e06f00c37b43fe5281d9ce973269496cc
Philippe Chartrand <philippe.chartrand@videotron.ca>
2019-02-23 17:55:27 -0500

    ajout des contrôles de mode et de température cible

--- static/js/components.js
+++ static/js/components.js
@@ -7,6 +7,48 @@ var statusComponent = new Vue({
         'automatic': ''
     },
     methods: {
+      setMode: function (){
+        var key;
+        if (statusComponent.$data.automatic){
+            key = 'manual';
+        }else{
+            key = 'automatic'
+        }
+        var action = '/'+ key;
+        axios.post(action, {action: true}, {headers: {'Content-Type': 'application/json'}})
+        .then(function (response) {
+            if (key == 'automatic'){
+                statusComponent.$data.automatic = true;
+            }else{
+                statusComponent.$data.automatic = false;
+            }
+            console.log('INFO '+response.status);
+        })
+        .catch(function(error){
+            console.log('WARN ' + error);
+        })
+        .then(function () {
+            // always executed
+            console.log('INFO setMode ' + key + ' done');
+        });
+      },
+      setTarget: function (){
+        axios.put('/target', {target: statusComponent.$data.target}, {headers: {'Content-Type': 'application/json'}})
+        .then(function (response) {
+            statusComponent.$data.temperature = response.data.temperature
+            statusComponent.$data.target = response.data.target
+            statusComponent.$data.heating = response.data.heating
+            statusComponent.$data.automatic = response.data.automatic
+            console.log('INFO '+response.status);
+        })
+        .catch(function(error){
+            console.log('WARN ' + error);
+        })
+        .then(function () {
+            // always executed
+            console.log('INFO setTarget done');
+        });
+      },
       poll: function () {
         axios.get('/status')
         .then(function (response) {
